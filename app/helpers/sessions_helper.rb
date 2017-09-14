c4aa0e72b5edb6b26465553aa94016170d06902c
Adam Weeks <aoweeks@gmail.com>
2017-09-14 19:20:47 +0000

    Complete basic login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,6 @@
 module SessionsHelper
   
+  #Logs in the given user
   def log_in(user)
     session[:user_id] = user.id
   end
@@ -8,4 +9,10 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+  
+  #Returns true if the user is logged in, false otherwise
+  def logged_in?
+    !current_user.nil?
+  end
+    
 end

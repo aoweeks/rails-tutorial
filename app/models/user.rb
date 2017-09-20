49d5e1580fdcc854499773e674ca0c5120b2d9a3
Adam Weeks <aoweeks@gmail.com>
2017-09-20 10:15:56 +0000

    Create account activation token/digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_create :create_activation_digest
+  before_save :downcase_email
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -8,8 +10,6 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  
-  attr_accessor :remember_token
   
   
   # Returns the hash digest of the given string.
@@ -40,4 +40,17 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  private
+  
+     # Converts email to all lower-case.
+    def downcase_email
+      email.downcase!
+    end
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end

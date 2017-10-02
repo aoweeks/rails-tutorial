f888f9072149e9aceb238f66d13a37495268c7fc
Adam Weeks <aoweeks@gmail.com>
2017-10-02 21:36:18 +0000

    Complete micropost posting mechanics

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-  get 'sessions/new'
-
   root 'static_pages#home'
   
   get  '/help',     to: 'static_pages#help'
@@ -13,13 +7,14 @@ Rails.application.routes.draw do
   
   get '/signup',    to: 'users#new'
   post '/signup',   to: 'users#create'
-  resources :users
   
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   
-  resources :account_activations, only: [:edit]
   
+  resources :users
+  resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
 end
